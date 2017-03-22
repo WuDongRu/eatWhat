c5e8fb7fec8a8ea12bbca2964f613c2d8eb7f4f4
BANK\M05368 <xx456258@gmail.com>
2017-03-22 17:28:25 +0800

    toolbar

--- app/src/main/java/com/example/m05368/eatwhat/MainActivity.java
+++ app/src/main/java/com/example/m05368/eatwhat/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.m05368.eatwhat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
